10829293d652a52477f05e59a6b47c3c8df2530f
luis <luis.mattes@universo.univates.br>
2022-09-15 06:50:16 -0300

    ptimeiro commit

---
+++ src/main/java/com/example/tela/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.tela;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    public Label conta;
+    @FXML
+    private Label senha;
+
+   /* @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }*/
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.tela {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.tela to javafx.fxml;
+    exports com.example.tela;
+}
